c57bb4bc06ce22e83ae1c11607f8fd225a7c1842
Harichinta <harivenkatesh1122@gmail.com>
2024-08-07 13:44:21 +0530

    navbar changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Home';
--- src/ComponentsModule/Searchbar/Searchbar.js
+++ src/ComponentsModule/Searchbar/Searchbar.js
@@ -6,8 +6,16 @@ function Searchbar() {
   const { state, setState } = useContext(DashboardContext);
 
   const Shownavbar=()=>{
+    const navbar = document.getElementById('navbarSupportedContent');
+    if (navbar.classList.contains('show')) {
+      navbar.classList.remove('show');
+    }
     setState(!state)
   }
+
+  const hidesidenav=()=>{
+    setState(false)
+  } 
 
   const serach =()=>{
     console.log("searching.......")
@@ -16,24 +24,28 @@ function Searchbar() {
     <div className='sidenavmain' >
       <nav className="navbar navbar-expand-lg navbar-light">
         <div className="container-fluid">
-         <div className='d-block d-md-none menubutton' onClick={()=>Shownavbar()}><i className="bi bi-microsoft"></i></div>
-         <button className="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <form className="d-flex w-25 position-relative serchform" >
+         <div className='d-block d-md-none menubutton' onClick={()=>Shownavbar()} ><i className="bi bi-microsoft"></i></div>
+          <div >
+          <form className="d-flex w-25 position-relative serchform" >
               <span onClick={serach}  className="position-absolute top-50 translate-middle-y" id="basic-addon1">
                 <i className="bi bi-search"></i>
               </span>
               <input className="inputsearch" style={{paddingLeft:"50px"}} type="search" placeholder="Search" aria-label="Search" />
             </form>
+          </div>
+          <button onClick={hidesidenav} className="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li><a href="mailto:harivenkateshjb12@gmail.com"><i className="bi bi-envelope"></i></a></li>
-              <li><a href="#"><i className="bi bi-gear"></i></a></li>
-              <li><a href="#"><i className="bi bi-bell-fill"></i></a></li>
-              <li><a><i className="bi bi-person-fill"></i></a></li>
+              <li><a href="mailto:harivenkateshjb12@gmail.com"><i className="bi bi-envelope"></i><span className='d-block d-md-none textspan'>harivenkateshjb12@gmail.com</span></a></li>
+              <li><a href="#"><i className="bi bi-gear"></i> <span className='d-block d-md-none textspan'>settings</span></a></li>
+              <li><a href="#"><i className="bi bi-bell-fill"></i> <span className='d-block d-md-none textspan'>notifications</span></a></li>
+              <li><a><i className="bi bi-person-fill"></i> <span className='d-block d-md-none textspan'>Account</span></a></li>
             </ul>
           </div>
+
+          
         </div>
       </nav>
     </div>
